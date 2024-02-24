e12eb0b4e87c173162d3b9a1b028b750a520ea7e
berat03 <bbulbulwork@gmail.com>
2024-02-24 19:10:29 +0000

    begin caving introduction page

--- frontend/src/pages/newToCaving.tsx
+++ frontend/src/pages/newToCaving.tsx
@@ -1,16 +1,8 @@
 import React from "react";
-import NavBar from "../components/navBar";
+import About from "../components/about";
 
 export default function NewToCaving() {
   return (
-      <div className="h-screen">
-        <div>
-            <NavBar/>
-        </div>
-
-          <h1 className='flex justify-center text-6xl font-extrabold'>NEW TO CAVING?!</h1>
-
-      </div>
-
+        <About/>
   );
 }
