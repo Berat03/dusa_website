8483f023a4888013857975941889f23d0cc95a73
berat03 <bbulbulwork@gmail.com>
2024-02-27 22:21:07 +0000

    try to center mailing list

--- frontend/src/components/mailingList.tsx
+++ frontend/src/components/mailingList.tsx
@@ -14,7 +14,7 @@
 */
 export default function MailingList() {
   return (
-    <div className="bg-white shadow sm:rounded-lg">
+    <div className="bg-white shadow sm:rounded-lg m-10">
       <div className="px-4 py-5 sm:p-6">
         <h3 className="text-base font-semibold leading-6 text-gray-900">Join our mailing list</h3>
         <div className="mt-2 max-w-xl text-sm text-gray-500">
--- frontend/src/pages/joinPage.tsx
+++ frontend/src/pages/joinPage.tsx
@@ -4,16 +4,16 @@ import ContactLinks from "../components/contactLinks";
 import MailingList from "../components/mailingList";
 
 export default function JoinPage() {
-  return (
-    <div className="h-screen">
-      <div>
-        <NavBar />
-      </div>
-        <div className="flex flex-col justify-center">
-            <ContactLinks />
-            <MailingList/>
-        </div>
+    return (
+        <div className="">
+            <div>
+                <NavBar/>
+            </div>
+            <div className="flex flex-col items-center">
+                <ContactLinks/>
+                    <MailingList/>
+            </div>
 
-    </div>
-  );
+        </div>
+    );
 }
