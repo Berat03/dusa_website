03ac4b8afebd8d28fd22e018c3c619fea834b14c
berat03 <bbulbulwork@gmail.com>
2024-02-27 21:57:39 +0000

    placeholder

---
+++ frontend/src/components/blogCarousel.tsx
@@ -0,0 +1,17 @@
+// thumbnail image for each blog, and then pull all the images when the blog is loaded
+    // higher resolution?
+
+import React, { useState } from 'react';
+
+
+
+export default function BlogCarousel() {
+  const [activeCard, setActiveCard] = useState(0);
+
+
+  return (
+    <div>
+        <h1>Hey</h1>
+    </div>
+  );
+}
