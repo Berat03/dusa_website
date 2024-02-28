266a3f5bfa5eef994fcf833a1439ee1d61602b58
berat03 <bbulbulwork@gmail.com>
2024-02-28 02:40:38 +0000

    remove excess padding

--- frontend/src/components/contactLinks.tsx
+++ frontend/src/components/contactLinks.tsx
@@ -31,7 +31,7 @@ export default function ContactLinks() {
     ];
 
     return (
-        <div className="isolate bg-white px-6 py-10 sm:py-10 lg:px-8 h-screen">
+        <div className="isolate bg-white px-6  sm:py-5 lg:px-8 h-screen">
             <div className="mx-auto max-w-2xl sm:text-center">
                 <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
                     Get Involved
