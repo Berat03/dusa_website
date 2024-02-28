d750ba514c2523afd78884185aac62abe4f59518
berat03 <bbulbulwork@gmail.com>
2024-02-28 22:48:51 +0000

    calender implement basic

--- frontend/src/pages/calenderPage.tsx
+++ frontend/src/pages/calenderPage.tsx
@@ -1,5 +1,6 @@
 import NavBar from "../components/navBar";
 import CalendarCalendar from "../components/calenderCalender";
+import CalenderCalender from "../components/calenderCalender";
 
 export default function CalenderPage() {
     return (
