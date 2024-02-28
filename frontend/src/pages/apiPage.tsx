9f5884ebd3a076e1b9475900794a5d1fb48f56a6
berat03 <bbulbulwork@gmail.com>
2024-02-28 22:48:41 +0000

    placeholder for api page

---
+++ frontend/src/pages/apiPage.tsx
@@ -0,0 +1,11 @@
+import NavBar from "../components/navBar";
+
+export default function ApiPage() {
+    return (
+        <div>
+            <NavBar/>
+            <h1>Page for others to use my own APIs</h1>
+        </div>
+
+    )
+}
