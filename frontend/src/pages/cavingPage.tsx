67c08585886bfcbae49ace543dcba5f8ec29b5a0
berat03 <bbulbulwork@gmail.com>
2024-02-28 22:50:05 +0000

    added image bg overlap text component

--- frontend/src/pages/cavingPage.tsx
+++ frontend/src/pages/cavingPage.tsx
@@ -3,17 +3,21 @@ import NavBar from "../components/navBar";
 import EquipmentCarousel from "../components/equipmentCarousel";
 import Footer from "../components/footer";
 import CavingBanner from "../components/cavingBanner";
-
+import ImageBgOverlapTextFull from "../components/imageBgOverlapTextFull";
+import Img from "../assets/me_stuck.jpg"
 
 export default function CavingPage() {
     return (
-        <div className="flex flex-col">
-            <NavBar/>
+        <div className="flex flex-col w-full bg-range-900">
+                <NavBar/>
             <div className="flex align-center h-screen">
                 <CavingBanner/>
             </div>
-            <div className="m-10 mt-10">
+            <div className="">
                 <EquipmentCarousel/>
+            </div>
+            <div>
+                <ImageBgOverlapTextFull title={"We are active weekly!"} image={Img} description={"blah blah brkrbn about caving blah blabh ablaegjeafnjef"}/>
             </div>
             <Footer/>
         </div>
