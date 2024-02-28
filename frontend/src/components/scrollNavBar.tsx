1337351644070241bceadfde4fba07b7d798a2e2
berat03 <bbulbulwork@gmail.com>
2024-02-28 01:59:55 +0000

    reformat prettier

--- frontend/src/components/scrollNavBar.tsx
+++ frontend/src/components/scrollNavBar.tsx
@@ -1,24 +1,24 @@
-import React, { useState, useEffect } from "react";
+import React, {useEffect, useState} from "react";
 import NavBar from "./navBar";
 
 export default function ScrollAwareNavBar() {
-  const [prevScrollPos, setPrevScrollPos] = useState<number>(0);
-  const [visible, setVisible] = useState<boolean>(true);
+    const [prevScrollPos, setPrevScrollPos] = useState<number>(0);
+    const [visible, setVisible] = useState<boolean>(true);
 
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
 
-    return () => { // clean dode
-      window.removeEventListener("scroll", handleScroll);
+        return () => { // clean dode
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, [prevScrollPos, visible]);
+
+    const handleScroll = () => {
+        const currentScrollPos = window.pageYOffset;
+        setVisible(prevScrollPos > currentScrollPos || currentScrollPos < 10);
+        setPrevScrollPos(currentScrollPos);
     };
-  }, [prevScrollPos, visible]);
 
-  const handleScroll = () => {
-    const currentScrollPos = window.pageYOffset;
-    setVisible(prevScrollPos > currentScrollPos || currentScrollPos < 10);
-    setPrevScrollPos(currentScrollPos);
-  };
-
-  return <NavBar  />;
+    return <NavBar/>;
 };
 
