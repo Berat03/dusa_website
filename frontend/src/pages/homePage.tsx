1b62fad0b359bca1563c6e00d389a2519191d3a3
berat03 <bbulbulwork@gmail.com>
2024-03-01 15:39:09 +0000

    fixed overlapping components below sm:

--- frontend/src/pages/homePage.tsx
+++ frontend/src/pages/homePage.tsx
@@ -4,20 +4,31 @@ import ClubStats from "../components/clubStats";
 import Footer from "../components/footer";
 import ImageTilesHalfPageText from "../components/imageTilesHalfPageText";
 import ScrollNavBar from "../components/scrollNavBar";
+import ImageGridDescription from "../components/imageGridDescription";
 
 // style={{backgroundColor:'rgb(10,20,28)'}}
 export default function HomePage() {
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-col ">
             <div className="sticky top-0 w-full z-50 ">
                 <ScrollNavBar/>
             </div>
             <div className="h-screen">
                 <ParallaxComposition/>
             </div>
-            <ClubStats/>
-            <ImageTilesHalfPageText/>
-            <Footer/>
+            <div>
+                <ImageGridDescription/>
+            </div>
+            <div className="mb-72 sm:mb-48 md:mb-0">
+                <ClubStats/>
+            </div>
+            <div >
+                <ImageTilesHalfPageText/>
+            </div>
+
+            <div>
+                <Footer/>
+            </div>
         </div>
     );
 }
