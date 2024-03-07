07e812e4c5810f02ded497261b7d70e27a22d0e7
berat03 <bbulbulwork@gmail.com>
2024-03-07 19:03:08 +0000

    tw aspect ratio

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,22 +1,25 @@
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
-  content: [
-
-    "./src/pages/*.{ts,tsx}",
-    "./src/components/*.{ts,tsx}"
-  ],
-  theme: {
-    extend: {
-      colors: {
-        'custom_bg': 'rgb(8,18,26)',
-      },
+    future: {
+        // removeDeprecatedGapUtilities: true,
+        // purgeLayersByDefault: true,
     },
-  },
-  variants: {},
-  plugins: [
-      require('@tailwindcss/forms'),
-],
+    content: [
+
+        "./src/pages/*.{ts,tsx}",
+        "./src/components/**/*.{ts,tsx}"
+
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'custom_bg': 'rgb(8,18,26)',
+            },
+        },
+    },
+    variants: {},
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/aspect-ratio'),
+
+    ],
 }
